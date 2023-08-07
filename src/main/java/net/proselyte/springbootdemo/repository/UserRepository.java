fbc6f84a9f6209508b3d7e11e41374bff75e366a
spezdmtest <spezd.m.@gmail.com>
2023-08-07 18:51:57 +0300

    Spring Boot CRUD

---
+++ src/main/java/net/proselyte/springbootdemo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package net.proselyte.springbootdemo.repository;
+
+import net.proselyte.springbootdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
---
+++ src/main/java/net/proselyte/springbootdemo/service/UserService.java
@@ -0,0 +1,30 @@
+package net.proselyte.springbootdemo.service;
+
+import lombok.AllArgsConstructor;
+import net.proselyte.springbootdemo.model.User;
+import net.proselyte.springbootdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
